da22e75bc107d8b8c2012cecda102959e4d2d30f
MEGAPLASTOR <dinht7124@gmail.com>
2026-06-26 01:04:45 +0700

    test v10

--- src/components/gift-code/layout/GiftPageDecorations.jsx
+++ src/components/gift-code/layout/GiftPageDecorations.jsx
@@ -3,11 +3,6 @@ export function GiftPageDecorations() {
     <>
       <div className="page-glow page-glow--left" aria-hidden="true" />
       <div className="page-glow page-glow--right" aria-hidden="true" />
-      <div className="floor-lines" aria-hidden="true">
-        <span />
-        <span />
-        <span />
-      </div>
     </>
   );
 }
