492856bb7e88c17bb6e057a2aa840718919eb5a0
MEGAPLASTOR <dinht7124@gmail.com>
2026-06-28 00:54:16 +0700

    Auto update Sun 06/28/2026  0:54:16.25

--- src/utils/giftCodeFormat.js
+++ src/utils/giftCodeFormat.js
@@ -1,14 +1,13 @@
-export const GIFT_CODE_EXAMPLE = "HDSPE_260622PSYR4BHX";
+export const GIFT_CODE_EXAMPLE = "2606168FJ15CHF";
 export const GIFT_CODE_FORMAT_LABEL =
-  "HDSPE_ + 6 chữ số + 8 ký tự chữ/số";
-export const GIFT_CODE_PATTERN = /^HDSPE_\d{6}[A-Z0-9]{8}$/;
+  "6 chữ số + 8 ký tự chữ/số";
+export const GIFT_CODE_PATTERN = /^\d{6}[A-Z0-9]{8}$/;
 
 export function formatGiftCodeInputValue(value) {
   return String(value || "")
     .toUpperCase()
     .replace(/\s+/g, "")
-    .replace(/-+/g, "_")
-    .replace(/_+/g, "_");
+    .replace(/[-_]+/g, "");
 }
 
 export function isValidGiftCodeFormat(value) {
